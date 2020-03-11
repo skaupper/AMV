87498be7e60fa577d71191f9b15fb883436cf6a9
Michael Wurm <wurm.michael95@gmail.com>
2020-03-11 12:27:53 +0100

    single read, single write

--- ue1/src/WishboneBFM_pack.vhd
+++ ue1/src/WishboneBFM_pack.vhd
@@ -16,19 +16,20 @@ package WishboneBFM_pack is
     -- Types
     --
 
-    type aBusIn is record
-        dat_i : std_ulogic_vector(cDataWidth-1 downto 0);
-        adr_i : std_ulogic_vector(cAddrWidth-1 downto 0);
-        sel_i : std_ulogic_vector(cDataWidth/8-1 downto 0);
-        cyc_i : std_ulogic;
-        stb_i : std_ulogic;
-        we_i  : std_ulogic;
-    end record;
+    type aBfmOut is record
+        dat : std_ulogic_vector(cDataWidth-1 downto 0);
+        adr : std_ulogic_vector(cAddrWidth-1 downto 0);
+        sel : std_ulogic_vector(cDataWidth/8-1 downto 0);
+        cyc : std_ulogic;
+        stb : std_ulogic;
+        we  : std_ulogic;
+    end record aBfmOut;
 
-    type aBusOut is record
-        dat_o : std_ulogic_vector(cDataWidth-1 downto 0);
-        ack_o : std_ulogic;
-    end record;
+    type aBfmIn is record
+        clk : std_ulogic;
+        dat : std_ulogic_vector(cDataWidth-1 downto 0);
+        ack : std_ulogic;
+    end record aBfmIn;
 
 
 
@@ -39,14 +40,14 @@ package WishboneBFM_pack is
     procedure busWrite (
         constant addr    : in  std_ulogic_vector(cAddrWidth-1 downto 0);
         constant data    : in  std_ulogic_vector(cDataWidth-1 downto 0);
-        signal   fromBus : in  aBusOut;
-        signal   toBus   : out aBusIn
+        signal   bfmOut  : out aBfmOut;
+        signal   bfmIn   : in  aBfmIn
     );
 
     procedure busRead (
         constant addr    : in  std_ulogic_vector(cAddrWidth-1 downto 0);
-        signal   fromBus : in  aBusOut;
-        signal   toBus   : out aBusIn;
+        signal   bfmOut  : out aBfmOut;
+        signal   bfmIn   : in  aBfmIn;
         variable data    : out std_ulogic_vector(cDataWidth-1 downto 0)
     );
 end package;
@@ -57,21 +58,41 @@ package body WishboneBFM_pack is
     procedure busWrite (
         constant addr    : in  std_ulogic_vector(cAddrWidth-1 downto 0);
         constant data    : in  std_ulogic_vector(cDataWidth-1 downto 0);
-        signal   fromBus : in  aBusOut;
-        signal   toBus   : out aBusIn
+        signal   bfmOut  : out aBfmOut;
+        signal   bfmIn   : in  aBfmIn
     ) is
     begin
-        -- TODO: implement
+        wait until rising_edge(bfmIn.clk);
+        bfmOut.adr <= addr;
+        bfmOut.dat <= data;
+        bfmOut.we <= '1';
+        bfmOut.sel <= (others => '1');
+        bfmOut.stb <= '1';
+        bfmOut.cyc <= '1';
+
+        wait until rising_edge(bfmIn.clk) and bfmIn.ack = '1';
+        bfmOut.stb <= '0';
+        bfmOut.cyc <= '0';
     end procedure;
 
     procedure busRead (
         constant addr    : in  std_ulogic_vector(cAddrWidth-1 downto 0);
-        signal   fromBus : in  aBusOut;
-        signal   toBus   : out aBusIn;
+        signal   bfmOut  : out aBfmOut;
+        signal   bfmIn   : in  aBfmIn;
         variable data    : out std_ulogic_vector(cDataWidth-1 downto 0)
     ) is
     begin
-        -- TODO: implement
+        wait until rising_edge(bfmIn.clk);
+        bfmOut.adr <= addr;
+        bfmOut.we <= '0';
+        bfmOut.sel <= (others => '1');
+        bfmOut.stb <= '1';
+        bfmOut.cyc <= '1';
+
+        wait until rising_edge(bfmIn.clk) and bfmIn.ack = '1';
+        data := bfmIn.dat;
+        bfmOut.stb <= '0';
+        bfmOut.cyc <= '0';
     end procedure;
 
 end package body;
