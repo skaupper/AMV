91960bb9a3c79c2059686517c21e6a6c88acc0bf
Michael Wurm <wurm.michael95@gmail.com>
2020-04-09 10:48:37 +0200

    wip

--- ue4/src/testbench.sv
+++ ue4/src/testbench.sv
@@ -47,3 +47,32 @@ program test (cpu_if.tb dut_if, output logic rst);
         $finish;
     end : stimuli
 endprogram
+
+
+`ifndef TYPES
+`define TYPES
+
+
+interface cpu_if(input bit clk);
+    logic[gDataWidth-1:0] mem_addr_o;
+    logic[gDataWidth-1:0] mem_data_i;
+    logic[gDataWidth-1:0] mem_data_o;
+    logic mem_ce_no;
+    logic mem_oe_no;
+    logic mem_we_no;
+    logic illegal_inst_o;
+    logic cpu_halt_o;
+
+
+    clocking cb @(posedge clk);
+        input  mem_data_i;
+
+        output mem_addr_o, mem_data_o, mem_ce_no,
+               mem_oe_no, mem_we_no, illegal_inst_o, cpu_halt_o;
+    endclocking
+
+    modport tb (clocking cb);
+endinterface
+
+
+`endif
