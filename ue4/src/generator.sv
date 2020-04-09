b5f8d27509d676b61c890bdc7d4686530ab440c8
Michael Wurm <wurm.michael95@gmail.com>
2020-04-09 18:21:10 +0200

    reg indexes in test cases

--- ue4/src/generator.sv
+++ ue4/src/generator.sv
@@ -34,30 +34,30 @@ class Generator;
       // tests.push_back(Prol16Opcode::create(LOAD, 1, 0));
       // tests.push_back(Prol16Opcode::create(STORE, 2, 3));
 
-      tests.push_back(Prol16Opcode::create(JUMP, 21));
-      tests.push_back(Prol16Opcode::create(SHL, 15));
-      tests.push_back(Prol16Opcode::create(JUMPC, 26));
-      tests.push_back(Prol16Opcode::create(JUMPZ, 31));
+      tests.push_back(Prol16Opcode::create(JUMP, 5));
+      tests.push_back(Prol16Opcode::create(SHL, 3));
+      tests.push_back(Prol16Opcode::create(JUMPC, 6));
+      tests.push_back(Prol16Opcode::create(JUMPZ, 7));
 
       tests.push_back(Prol16Opcode::create(COMP, 0, 0));
 
-      tests.push_back(Prol16Opcode::create(JUMP, 10));
-      tests.push_back(Prol16Opcode::create(JUMPC, 15));
-      tests.push_back(Prol16Opcode::create(JUMPZ, 20));
+      tests.push_back(Prol16Opcode::create(JUMP, 4));
+      tests.push_back(Prol16Opcode::create(JUMPC, 1));
+      tests.push_back(Prol16Opcode::create(JUMPZ, 2));
 
-      tests.push_back(Prol16Opcode::create(MOVE, 5, 15));
-      tests.push_back(Prol16Opcode::create(AND, 14, 3));
-      tests.push_back(Prol16Opcode::create(OR, 13, 6));
-      tests.push_back(Prol16Opcode::create(XOR, 12, 11));
-      tests.push_back(Prol16Opcode::create(NOT, 11));
+      tests.push_back(Prol16Opcode::create(MOVE, 5, 0));
+      tests.push_back(Prol16Opcode::create(AND, 6, 3));
+      tests.push_back(Prol16Opcode::create(OR, 4, 2));
+      tests.push_back(Prol16Opcode::create(XOR, 0, 1));
+      tests.push_back(Prol16Opcode::create(NOT, 7));
 
-      tests.push_back(Prol16Opcode::create(ADD, 3, 12));
+      tests.push_back(Prol16Opcode::create(ADD, 3, 1));
       tests.push_back(Prol16Opcode::create(ADDC, 1, 2));
       tests.push_back(Prol16Opcode::create(SUBC, 2, 3));
-      tests.push_back(Prol16Opcode::create(SUB, 11, 7));
+      tests.push_back(Prol16Opcode::create(SUB, 0, 7));
 
-      tests.push_back(Prol16Opcode::create(COMP, 3, 12));
-      tests.push_back(Prol16Opcode::create(INC, 15));
+      tests.push_back(Prol16Opcode::create(COMP, 3, 4));
+      tests.push_back(Prol16Opcode::create(INC, 7));
       tests.push_back(Prol16Opcode::create(DEC, 1));
 
       tests.push_back(Prol16Opcode::create(SHL, 3));
