6e292094a0ac3fc59ac4709e4705c0905dedab76
Michael Wurm <wurm.michael95@gmail.com>
2020-05-01 11:34:21 +0200

    prio constraints

--- ue6/src/model/Prol16Opcode.sv
+++ ue6/src/model/Prol16Opcode.sv
@@ -55,6 +55,11 @@ class Prol16Opcode;
         } -> (rb == 0);
     }
 
+    constraint prio_cmd {
+        solve cmd before ra;
+        solve cmd before rb;
+    }
+
 
     function new();
         setAll(NOP);
