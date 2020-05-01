37af6650450da6530ffd22c537f5dcface003de3
Michael Wurm <wurm.michael95@gmail.com>
2020-05-01 11:28:47 +0200

    constrain no_reg again

--- ue6/src/model/Prol16Opcode.sv
+++ ue6/src/model/Prol16Opcode.sv
@@ -43,11 +43,11 @@ class Prol16Opcode;
 
     constraint ignore_cmds { !(cmd inside {SLEEP, STORE, LOAD}); }
 
-    // constraint no_reg_used {
-    //     cmd inside {
-    //         NOP, SLEEP
-    //     } -> (ra == 0 && rb == 0);
-    // }
+    constraint no_reg_used {
+        cmd inside {
+            NOP, SLEEP
+        } -> (ra == 0 && rb == 0);
+    }
 
     // constraint only_one_reg_used {
     //     cmd inside {
